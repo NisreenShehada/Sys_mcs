877548e8426d0c773cc65ac0f7d0cd17fda13846
NisreenShehada <nms24@ic.ac.uk>
2026-04-23 12:19:48 +0200

    updated R1

--- analysis/01_download_included_studies.R
+++ analysis/01_download_included_studies.R
@@ -1,10 +1,32 @@
-# Download the screened studies and filter the included studies
+# 1. Download the screened studies and filter the included studies
 
-# Identify an R pacakge to help download from google drive
+# 2. Identify an R pacakge to help download from google drive
 install.packages("googledrive")
+library(googledrive)
 
-# Download the file
+# Authenticate with Google
+drive_auth()
 
+# 3. Download the file
+folder <- drive_get("PDFs Zotero")
+
+# List files
+files <- drive_ls(folder)
+
+# Download all PDFs
+
+pdf_files <- subset(files, grepl("\\.pdf$", name,
+    ignore.case = TRUE))
+
+for (i in seq_len(nrow(pdf_files))) {
+  drive_download(
+    file = pdf_files[i, ],
+    path = file.path ("analysis/data-raw/", pdf_files$name[i]),
+    overwrite = TRUE
+  )
+}
+  )
+}
 # Checks on the file
 
 # Filter the file
